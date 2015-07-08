44dfc6e8efe6ff2736ab0f13ea61fba211a5c65f
Phil <phil@unknown.home>
2015-07-08 17:39:42 +0100

    Addition of Apache 2 licence to EMBL-EBI created code. Removal from class.database.php of all sensitive database information. This now needs to be held inside inc_db_confog.php which is NEVER under version control.

--- emmaSearch/mut_types.php
+++ emmaSearch/mut_types.php
@@ -1,4 +1,25 @@
 <?php 
+
+#===================
+# LICENCE
+#===================
+
+/*
+
+Copyright 2015 EMBL-EBI
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is 
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+
+See the License for the specific language governing permissions and limitations under the License.
+
+*/
+
 $mutype= array(
 	      "TM"=>"Targeted Mutant Strains", 
 	      "TMKO"=>"Knock-out",
@@ -18,10 +39,11 @@ $mutype= array(
 	      "SP"=>"Spontaneous",
 	      "XX"=>"Other",
 	      "Cre"=>"expressing Cre Recombinase",
+	      "EUCre"=>"EUCOMMTools Cre strains",
 	      "FLP"=>"expressing Flp Recombinase",
 	      "TET"=>"with Tet expression system",
 	      "DEL"=>"Deltagen strains",
 	      "LEX"=>"Lexicon strains",
 	      "EUC"=>"IKMC/IMPC strains",
-"EUCre"=>"EUCOMMTools Cre strains",)
+)
 ?>
