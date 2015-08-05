0dbefd83e07439816af73e675b58e6542b22abd8
Phil <phil@Phils-MacBook-Pro-2.local>
2015-08-05 12:41:33 +0100

    Changes to allow movement to public GitHub

--- emmaSearch/class.database.php
+++ emmaSearch/class.database.php
@@ -1,28 +1,52 @@
 <?php
+
+#===================
+# LICENCE
+#===================
+
+/*
+
+Copyright 2015 EMBL-EBI
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is 
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+
+See the License for the specific language governing permissions and limitations under the License.
+
+*/
    
 class database {
-  
-    private $server="mysql-emmastr:4167";
-    private $database="emmastr";
-    private $user="admin";
-    private $password="qGUZ1dx4";   
+    private $server="";
+    private $database="";
+    private $user="";
+    private $password=""; 
 
     function __construct($user="",$password="",$server="",$database="")  {
-        
+    require_once('inc_db_config.php');
+    $this->server=HOST . ":" . PORT;
+            $this->database=DATABASE;
+            $this->user=USER;
+            $this->password=PASSWD;    
     	if($user!="")    {$this->user=$user;}
         if($password!=""){$this->password=$password;}
         if($server!="")  {$this->server=$server;}
         if($database!=""){$this->database=$database;}        
-       
+# Now not necessary, left for historical purposes
         $domain = $_SERVER['HTTP_HOST'];
         if ( $domain == 'localdev.infrafrontier.eu' ){
-            $this->server="localhost:3306";
-            $this->database="emmastr";
-            $this->user="root";
-            $this->password="a";
+            $this->server=HOST . ":" . PORT;
+            $this->database=DATABASE;
+            $this->user=USER;
+            $this->password=PASSWD;
         }
     }
 	function db_fetch($query) {
+error_log("SQL IS::$query", 0);
 		$dbconn = mysql_connect($this->server,$this->user,$this->password);
         mysql_select_db($this->database,$dbconn);    
                 
